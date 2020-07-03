abc665075e1fd96cf52bfad854adeb764f12d990
jrucl2d <ysk789465@gmail.com>
2020-07-04 00:20:53 +0900

    Modifying Search Drop bar

--- Github_clone/main.js
+++ Github_clone/main.js
@@ -4,23 +4,20 @@ const body = document.querySelector("body");
 function init() {
   const searchedHistory = document.querySelector(".search_history");
   const searchBar = document.querySelector(".js-searchBar");
-  let searchBarClicked = false;
+  let clicked = false;
 
   function showSeachHistory() {
+    clicked = true;
+    if (searchedHistory.classList.contains("show")) {
+      return undefined;
+    }
     searchedHistory.classList.toggle("show");
     searchBar.classList.toggle("long");
-    searchBarClicked = searchBarClicked ? false : true; // 같이 토글
   }
 
   searchBar.addEventListener("click", showSeachHistory);
 
-  body.addEventListener("mousedown", () => {
-    if (!searchBarClicked) {
-      return undefined; // 안 보이는 상태였으면 노 상관
-    } else {
-      showSeachHistory(); // 보이는 상태였으면 토글
-    }
-  });
+  body.addEventListener("mousedown", () => {});
 }
 
 init();
