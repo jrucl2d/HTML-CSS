02248c8ef8c137850c55036a76cda83626a9f1cf
jrucl2d <ysk789465@gmail.com>
2020-07-04 00:48:07 +0900

    Search Bar Dropdown done

--- Github_clone/main.js
+++ Github_clone/main.js
@@ -4,20 +4,32 @@ const body = document.querySelector("body");
 function init() {
   const searchedHistory = document.querySelector(".search_history");
   const searchBar = document.querySelector(".js-searchBar");
+  const hideIcon = document.querySelector(".header-search-key-slash");
   let clicked = false;
 
   function showSeachHistory() {
-    clicked = true;
-    if (searchedHistory.classList.contains("show")) {
-      return undefined;
-    }
     searchedHistory.classList.toggle("show");
     searchBar.classList.toggle("long");
+    hideIcon.classList.toggle("showSearchIcon");
   }
 
-  searchBar.addEventListener("click", showSeachHistory);
+  searchBar.addEventListener("click", () => {
+    if (searchedHistory.classList.contains("show")) {
+      // 검색창이 이미 켜져있는데 클릭했으면
+      return undefined;
+    }
+    showSeachHistory();
+  });
 
-  body.addEventListener("mousedown", () => {});
+  body.addEventListener("mousedown", (event) => {
+    if (event.target.classList.contains("long")) {
+      // 1. 검색 창 클릭
+      return undefined;
+    } else if (searchedHistory.classList.contains("show")) {
+      // 2. 다른 곳 클릭했으나 검색창 켜져있으면
+      showSeachHistory();
+    }
+  });
 }
 
 init();
