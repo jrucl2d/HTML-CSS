15b691c7065f182799829817874744dd479f7b1e
jrucl2d <ysk789465@gmail.com>
2020-07-11 18:08:33 +0900

    slider finished

--- InteractiveSites_clone/js/slider.js
+++ InteractiveSites_clone/js/slider.js
@@ -1,2 +1,29 @@
-function init() {}
+function init() {
+  const left = document.querySelector(".left");
+  const right = document.querySelector(".right");
+
+  let num = 0;
+  left.addEventListener("click", () => {
+    const showImg = document.querySelector(".show");
+    const select = document.querySelector(".select");
+    showImg.classList.remove("show");
+    select.classList.remove("select");
+    num = num - 1 == -1 ? 4 : num - 1;
+    const newShowImg = Array.from(document.querySelectorAll("img"));
+    const newSelect = Array.from(document.querySelectorAll(".btns"));
+    newShowImg[num].classList.add("show");
+    newSelect[num].classList.add("select");
+  });
+  right.addEventListener("click", () => {
+    const showImg = document.querySelector(".show");
+    const select = document.querySelector(".select");
+    showImg.classList.remove("show");
+    select.classList.remove("select");
+    num = num + 1 == 5 ? 0 : num + 1;
+    const newShowImg = Array.from(document.querySelectorAll("img"));
+    const newSelect = Array.from(document.querySelectorAll(".btns"));
+    newShowImg[num].classList.add("show");
+    newSelect[num].classList.add("select");
+  });
+}
 init();
