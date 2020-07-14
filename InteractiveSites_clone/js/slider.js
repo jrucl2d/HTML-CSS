4537a8089fb488a67106a15da5fc82f45394e70a
jrucl2d <ysk789465@gmail.com>
2020-07-14 22:59:01 +0900

    Slider window changed -final

--- InteractiveSites_clone/js/slider.js
+++ InteractiveSites_clone/js/slider.js
@@ -1,29 +1,43 @@
 function init() {
   const left = document.querySelector(".left");
   const right = document.querySelector(".right");
-
+  const btns = document.querySelector(".btns");
   let num = 0;
-  left.addEventListener("click", () => {
+
+  function Change(newNum) {
+    // 새로운 이미지로 변경하는 함수
     const showImg = document.querySelector(".show");
     const select = document.querySelector(".select");
     showImg.classList.remove("show");
     select.classList.remove("select");
-    num = num - 1 == -1 ? 4 : num - 1;
+    num = newNum; // 선택된 숫자로 갱신
     const newShowImg = Array.from(document.querySelectorAll("img"));
-    const newSelect = Array.from(document.querySelectorAll(".btns"));
+    const newSelect = Array.from(document.querySelectorAll(".btn"));
     newShowImg[num].classList.add("show");
     newSelect[num].classList.add("select");
+  }
+
+  function BtnClickHandler(event) {
+    // 클릭한 버튼의 아이디 값을 가져옴
+    const tar = event.target;
+    const clickedNum = parseInt(tar.getAttribute("id"));
+    if (num == clickedNum) {
+      // 이미 선택된 것이면 리턴
+      return null;
+    }
+    // 기존의 선택된 것은 제거
+    Change(clickedNum);
+  }
+
+  left.addEventListener("click", () => {
+    newNum = num - 1 == -1 ? 4 : num - 1;
+    Change(newNum);
   });
   right.addEventListener("click", () => {
-    const showImg = document.querySelector(".show");
-    const select = document.querySelector(".select");
-    showImg.classList.remove("show");
-    select.classList.remove("select");
-    num = num + 1 == 5 ? 0 : num + 1;
-    const newShowImg = Array.from(document.querySelectorAll("img"));
-    const newSelect = Array.from(document.querySelectorAll(".btns"));
-    newShowImg[num].classList.add("show");
-    newSelect[num].classList.add("select");
+    newNum = num + 1 == 5 ? 0 : num + 1;
+    Change(newNum);
   });
+  // 이벤트 위임 연습
+  btns.addEventListener("click", BtnClickHandler);
 }
 init();
