a14056c05675547631e66337689fd3047e041489
jrucl2d <ysk789465@gmail.com>
2020-07-14 21:55:26 +0900

    menu window changed and done

--- InteractiveSites_clone/js/menu.js
+++ InteractiveSites_clone/js/menu.js
@@ -1,7 +1,9 @@
 function init() {
   const btn = document.querySelector(".btn");
+  const menu = document.querySelector(".menu");
   btn.addEventListener("click", () => {
     btn.classList.toggle("btnclicked");
+    menu.classList.toggle("menuShow");
   });
 }
 init();
